ec03dc5d1849a18b3e754135a715c6845f2960b4
Fe1ch <linkin-park_94@mail.ru>
2024-02-07 13:17:58 +0300

    add storybook, loki

--- .eslintrc.js
+++ .eslintrc.js
@@ -41,10 +41,13 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error', {
-            markupOnly: true,
-            ignoreAttribute: ['data-testing'],
-        }],
+        'i18next/no-literal-string': [
+            'error',
+            {
+                markupOnly: true,
+                ignoreAttribute: ['data-testing', 'to'],
+            },
+        ],
         'max-len': ['error', { ignoreComments: true, code: 100 }],
     },
     globals: {
