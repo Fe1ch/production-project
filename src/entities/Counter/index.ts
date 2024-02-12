603d5f328482a6b0dcb2153e4f472726c3812471
Fe1ch <linkin-park_94@mail.ru>
2024-02-12 10:44:34 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/index.ts
@@ -0,0 +1,9 @@
+import { counterReducer } from './model/slice/counterSlise';
+import { Counter } from './ui/Counter';
+import { CounterSchema } from './model/types/counterSchema';
+
+export {
+    counterReducer,
+    Counter,
+    CounterSchema,
+};
