4a3b9a6bca8f54deb57877237da07676e0d95144
Fe1ch <linkin-park_94@mail.ru>
2024-03-30 17:22:57 +0300

    add skeleton , code, article details card

--- src/shared/ui/Text/Text.tsx
+++ src/shared/ui/Text/Text.tsx
@@ -2,9 +2,9 @@ import { classNames, Mods } from 'shared/lib/classNames/classNames';
 import { memo } from 'react';
 import cls from './Text.module.scss';
 
-export enum TextTheme{
-    PRIMARY = 'primary',
-    ERROR = 'error',
+export enum TextTheme {
+  PRIMARY = 'primary',
+  ERROR = 'error',
 }
 
 export enum TextAlign{
@@ -13,12 +13,18 @@ export enum TextAlign{
     CENTER = 'center',
 }
 
+export enum TextSize{
+    M = 'size_m',
+    L = 'size_l',
+}
+
 interface TextProps {
     className?: string;
     title?: string;
     text?: string;
     theme?: TextTheme;
     align?: TextAlign;
+    size?:TextSize;
 }
 
 export const Text = memo((props: TextProps) => {
@@ -28,11 +34,13 @@ export const Text = memo((props: TextProps) => {
         text,
         theme = TextTheme.PRIMARY,
         align = TextAlign.LEFT,
+        size = TextSize.M,
     } = props;
 
     const mods:Mods = {
         [cls[theme]]: true,
         [cls[align]]: true,
+        [cls[size]]: true,
     };
     return (
         <div className={classNames(cls.Text, mods, [className])}>
